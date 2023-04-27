54fa52f380d279918c53c24196ecfc8082c71d09
Hugo Marcelo Noguera <hugomarcelo.noguera@gmail.com>
2023-04-26 20:58:30 -0400

    Add files via upload

---
+++ st_days11.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What is your favorite color?',
+     (['Green','Yellow', 'Red', 'Blue'],['Yellow','Red'])
+
+     st.write('You selected: ', options)
